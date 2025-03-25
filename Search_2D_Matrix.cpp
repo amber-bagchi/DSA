7ccf8fa286f9926107a7acb95e69fefa5bc4f677
Amber_Bagchi <amberbagchi.work@gmail.com>
2025-03-25 12:35:01 +0530

    updated log(n*m)

--- Search_2D_Matrix.cpp
+++ Search_2D_Matrix.cpp
@@ -3,32 +3,41 @@
 
 using namespace std;
 
-bool Search(vector<int>& arr, int m, int k) {
+bool Search(vector<int>& arr, int m, int k){
     int s = 0;
     int e = m - 1;
     bool ans = false;
 
-    while (s <= e) {
-        int mid = s + (e - s) / 2;
-        if (arr[mid] == k) return true;
-        if (arr[mid] > k) e = mid - 1;
+    while(s<=e){
+        int mid = s + (e-s)/2;
+        if(arr[mid]==k) return true;
+        if(arr[mid]>k) e = mid - 1;
         else s = mid + 1;
     }
     return ans;
 }
 
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    bool ans = false;
-    for (int i = 0; i < matrix.size(); i++) {
-        int m = matrix[i].size();
-        if (Search(matrix[i], m, target)) {
-            ans = true;
+    
+    int n = matrix.size();
+    int s = 0;
+    int e = n - 1;
+    int m = matrix[0].size();
+    while(s<=e){
+        int mid = s + (e-s)/2;
+
+        if(target>=matrix[mid][0] && target<=matrix[mid][m-1]){
+            return Search(matrix[mid],m,target);
         }
-        if (ans == true) {
-            return ans;
+        else if(target>=matrix[mid][m-1]){
+            s = mid + 1;
+        }
+        else{
+            e = mid - 1;
         }
     }
-    return ans;
+    
+    return false;
 }
 
 int main() {
